0c5e49a8b87d2eda8e7527e09de97386b133aeff
Eric Pietraszkiewicz <erp2151@columbia.edu>
2026-08-06 07:37:06 -0700

    branding pt 3

--- src/panels/map/MapHeader.tsx
+++ src/panels/map/MapHeader.tsx
@@ -7,16 +7,7 @@ type MapHeaderProps = {
 
 export function MapHeader({ hideUserSection = false }: MapHeaderProps) {
   return (
-    <Panel className="flex h-header w-full min-w-0 shrink-0 items-center justify-between gap-4 border-0 bg-transparent">
-      <div className="flex shrink-0 items-center">
-        <img
-          src="/brand/ocumap-o-logo.svg"
-          alt="OcuMap"
-          className="h-9 w-auto"
-          width={33}
-          height={40}
-        />
-      </div>
+    <Panel className="flex h-header w-full min-w-0 shrink-0 items-center justify-end gap-4 border-0 bg-transparent">
       {hideUserSection ? null : <UserAccountDisplay />}
     </Panel>
   )
